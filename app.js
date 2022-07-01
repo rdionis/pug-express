b0816427dc9aa40ac549cfbf3fda75db8389133d
Raquel Dionísio <dionisio@stadtwerk.org>
2022-07-01 14:14:04 +0200

    commenting the code

--- app.js
+++ app.js
@@ -6,5 +6,7 @@ const PORT = 3000
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
+// Here, we are declaring a route (/), which will respond to a GET request with the text “Hello, World!”
 
 app.listen(3000, () => console.log(`Server listening on ${PORT}` ))
+// We can test this in our browsers, by starting the server with node app.js and then visiting http://localhost:3000.
