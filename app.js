da234881cbfbfb0035824933fea27b5f62a66259
Raquel Dionísio <dionisio@stadtwerk.org>
2022-07-01 15:03:20 +0200

    updated app.js with axios

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express = require('express');
+const axios = require('axios')
 const app = express();
+
+app.set('view engine', 'pug')
 
 const PORT = 3000
 
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
+app.get('/', async(req, res) => {
+    const query = await axios.get('http://localhost:3001/results')
+    res.render('index', {employees: query.data});
 });
 // Here, we are declaring a route (/), which will respond to a GET request with the text “Hello, World!”
+
 
 app.listen(3000, () => console.log(`Server listening on ${PORT}` ))
 // We can test this in our browsers, by starting the server with node app.js and then visiting http://localhost:3000.
